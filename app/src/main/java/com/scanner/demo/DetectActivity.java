bcf0c82c666d60225cdcd5e77629214f91ab5bd4
SoeunLee <soeun2327@gmail.com>
2016-12-03 22:58:54 +0900

    Detect 레이아웃에 Binarization 버튼 생성

--- app/src/main/java/com/scanner/demo/DetectActivity.java
+++ app/src/main/java/com/scanner/demo/DetectActivity.java
@@ -8,12 +8,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
 /**
  * Created by soeun on 2016. 12. 3..
  */
 
 public class DetectActivity extends Activity {
     private ImageView procImage;
+    private Bitmap bitmap;
+    private Mat matDst, matSrc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +30,15 @@ public class DetectActivity extends Activity {
         String route = intent.getStringExtra("SCANNED_IMAGE");
         BitmapFactory.Options bfo = new BitmapFactory.Options();
         bfo.inSampleSize = 2;
-        Bitmap bitmap = BitmapFactory.decodeFile(route, bfo);
+        bitmap = BitmapFactory.decodeFile(route, bfo);
         //Bitmap resized = Bitmap.createScaledBitmap(bitmap, imgWidth, imgHeight, true);
 
         if(route!=null) procImage.setImageBitmap(bitmap);
             Log.e("Soeun", route);
+
+        matSrc = new Mat();
+        Utils.bitmapToMat(bitmap, matSrc);
+        matDst = matSrc.clone();
+        Utils.adaptiveThreshold();
     }
 }

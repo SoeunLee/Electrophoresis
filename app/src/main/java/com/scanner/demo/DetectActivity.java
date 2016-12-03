0db0f5832392e702d6ec2ce1264ef2ec88870721
SoeunLee <soeun2327@gmail.com>
2016-12-04 01:21:36 +0900

    Binarization 수정, Original 버튼 생

--- app/src/main/java/com/scanner/demo/DetectActivity.java
+++ app/src/main/java/com/scanner/demo/DetectActivity.java
@@ -13,6 +13,7 @@ import android.widget.ImageView;
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 /**
@@ -22,8 +23,9 @@ import org.opencv.imgproc.Imgproc;
 public class DetectActivity extends Activity {
     private ImageView procImage;
     private Button histButton;
-    private Bitmap bitmap;
-    private Mat mat;
+    private Button originButton;
+    private Bitmap bitmap, bitmapOrigin;
+    private Mat mat, matOrigin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +34,15 @@ public class DetectActivity extends Activity {
         procImage = (ImageView) findViewById(R.id.proc_image);
         histButton = (Button) findViewById(R.id.hist_button);
         histButton.setOnClickListener(new histButtonOnClickListener());
+        originButton = (Button) findViewById(R.id.origin_button);
+        originButton.setOnClickListener(new originButtonOnClickListener());
 
         Intent intent = getIntent();
         String route = intent.getStringExtra("SCANNED_IMAGE");
         //BitmapFactory.Options bfo = new BitmapFactory.Options();
         //bfo.inSampleSize = 2;
         bitmap = BitmapFactory.decodeFile(route);
+        bitmapOrigin = bitmap.copy(bitmap.getConfig(), true);
         //Bitmap resized = Bitmap.createScaledBitmap(bitmap, imgWidth, imgHeight, true);
 
         if(route!=null) procImage.setImageBitmap(bitmap);
@@ -47,28 +52,42 @@ public class DetectActivity extends Activity {
         mat = new Mat (bitmap.getHeight(), bitmap.getWidth(), CvType.CV_8UC1);
         Utils.bitmapToMat(bitmap, mat);
         Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
+        matOrigin = mat.clone();
     }
     private class histButtonOnClickListener implements View.OnClickListener {
         private boolean isFirst;
+        private int blockSize;
+        private int constant;
 
         histButtonOnClickListener(){
-            isFirst = true;
+
+            blockSize = 63;
+            constant = 10;
         }
         @Override
         public void onClick(View v) {
-            if(isFirst) {
-                isFirst = false;
-                Imgproc.medianBlur(mat, mat, 5);
-                Imgproc.adaptiveThreshold(
-                        mat, mat, 255,
-                        Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C,
-                        Imgproc.THRESH_BINARY_INV, 7, 3);
+            Imgproc.medianBlur(mat, mat, 9);
+            Imgproc.blur(mat, mat, new Size(3, 3));
+ //         Imgproc.equalizeHist(mat, mat);
+            Imgproc.adaptiveThreshold(mat, mat, 255,
+                    Imgproc.ADAPTIVE_THRESH_MEAN_C,
+                    Imgproc.THRESH_BINARY, blockSize, constant);
 
-                Utils.matToBitmap(mat, bitmap);
-                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
-                procImage.setImageBitmap(bitmap);
-                procImage.invalidate();
-            }
+            Utils.matToBitmap(mat, bitmap);
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+            procImage.setImageBitmap(bitmap);
+            procImage.invalidate();
+
+        }
+    }
+    private class originButtonOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            bitmap = bitmapOrigin.copy(bitmapOrigin.getConfig(), true);
+            mat = matOrigin.clone();
+            procImage.setImageBitmap(bitmap);
+            procImage.invalidate();
         }
     }
 }
